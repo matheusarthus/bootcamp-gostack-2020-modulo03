f4cede2526f106c7b13547fbcffdafd2417370a8
Matheus Alves Arthus <matheus.arthus@gmail.com>
2020-03-17 16:16:01 -0300

    feature: api alterada p/ utilização do GoBarberWeb

--- src/app/controllers/ScheduleControler.js
+++ src/app/controllers/ScheduleControler.js
@@ -28,7 +28,14 @@ class ScheduleController {
           [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
         },
       },
-      orde: ['date'],
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
+      order: ['date'],
     });
 
     return res.json(appointments);
