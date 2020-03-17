f2479dfd9d56897b219be8c7a728561117a70435
Matheus Alves Arthus <matheus.arthus@gmail.com>
2020-03-17 15:30:20 -0300

    chore: api finalizada

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '42fb6affe49d265dfe5ed06b8d96b320',
+  expiresIn: '7d',
+};
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'eb7fb06262358c',
+    pass: '85822b47f4c04d',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
